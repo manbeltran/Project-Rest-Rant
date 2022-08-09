1b9412cccccd2735e4f68d4d6b67468ca39d0d67
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-08 20:24:08 -0700

    adding places view with mock data

--- index.js
+++ index.js
@@ -11,8 +11,23 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+// GET /places  
+
 app.get('*', (req, res) => {
     res.render('error404')
 })
 
 app.listen(process.env.PORT)
+
+
+/*
+3. Repeat the process above that you followed for creating a view for both the home page and 404 page.
+
+
+
+Import React and your default component.
+Make a function called index and fill it with some stub HTML.
+Export the index function.
+If you forgot how to do these steps, refer to the directions you followed for the 404 page or the home page.
+
+*/
