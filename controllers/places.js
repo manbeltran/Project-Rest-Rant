40329801d6f0a6a09d4b3b90a71fe5ea1f3b4284
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-10 20:42:17 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
-const rests = require("../models/rests.js")
+const places = require('../models/places.js')
+const rests = require("../models/places.js")
 
 
 router.get('/new', (req,res) => {
@@ -9,19 +10,19 @@ router.get('/new', (req,res) => {
 
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-coffee.jpg'
-      }]
+    // let places = [{
+    //     name: 'H-Thai-ML',
+    //     city: 'Seattle',
+    //     state: 'WA',
+    //     cuisines: 'Thai, Pan-Asian',
+    //     pic: '/images/thai-food.jpg'
+    //   }, {
+    //     name: 'Coding Cat Cafe',
+    //     city: 'Phoenix',
+    //     state: 'AZ',
+    //     cuisines: 'Coffee, Bakery',
+    //     pic: '/images/cat-coffee.jpg'
+    //   }]
       
     res.render('places/index', { places })
   })
@@ -34,11 +35,23 @@ router.get('/', (req, res) => {
 //CREATE
 
 router.post('/', (req, res) => {
-  
   console.log(req.body)
-  res.send('POST /places')
-  //res.redirect('/places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
+
+
 
 // router.get('/places', (req, res) => {
   
