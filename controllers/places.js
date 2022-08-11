0447b6782335004d8646d2354bc5629f6c9111d2
Jesus Beltran <jesusbeltran@Jesuss-MacBook-Pro.local>
2022-08-11 09:35:08 -0700

    Show route added

--- controllers/places.js
+++ controllers/places.js
@@ -50,6 +50,22 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+//Show
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
+
 
 
 
