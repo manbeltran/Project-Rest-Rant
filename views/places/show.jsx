389250cdc78e1c74445ab26c62a463b59c7316dd
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-24 00:50:12 -0700

    comment functionality not up

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -65,16 +65,33 @@ function show (data) {
             </div>
 
             <h2>Comments</h2>
-            {comments}
-            <div className='row'>
+            <div className="row">
               {comments}
             </div>
-
-            <h2>Got a comment?</h2>
-            <form action={`/places/${data.place.id}/comment`} method ="POST">
-              <div>
-                Form
+            <hr />
+            <h2>Got Your Own Rant or Rave?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label for="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
               </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label for="author">Author</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label for="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label for="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
             </form>
 
             
